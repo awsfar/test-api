5a75e3843add5f9c3f55d474d3ff842a6583ec19
awsfar <160767326+awsfar@users.noreply.github.com>
2024-05-07 12:35:07 +0100

    Update ap.js

--- dist2/ap.js
+++ dist2/ap.js
@@ -1,1 +1,13 @@
+// api/hello.js
+import { VercelRequest, VercelResponse } from '@vercel/node'
+import fs from 'fs'
+import path from 'path'
 
+export default function handler(req: VercelRequest, res: VercelResponse) {
+  const { name = 'World' } = req.query
+  const filePath = path.join(process.cwd(), 'public', 'index.html')
+  const fileContents = fs.readFileSync(filePath, 'utf8')
+
+  res.setHeader('Content-Type', 'text/html')
+  res.send(fileContents.replace('World', name))
+}
